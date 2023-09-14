94cdde07548f0a9886c17d8a64f9f46afaf88e8f
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-09-14 14:55:00 +0300

    Refactor

--- PrefabIconGenerator.gd
+++ PrefabIconGenerator.gd
@@ -49,14 +49,115 @@ var curveSidewaysRange = [endOffsetSize, endOffsetSize * 2, endOffsetSize * 3]
 
 var started: int = 3
 var curve: bool = false
+
+func _ready():
+	var dir = DirAccess.open("user://")
+	if dir.dir_exists("user://prefabIcons"):
+		pass
+
+var length
+var endOffset
+var leftHeight
+var rightHeight
+var startWall
+var endWall
+var curveForward
+var curveSideways
+
+func prepareVariables():
+	length = lengthRange[lengthIndex]
+	endOffset = endOffsetRange[endOffsetIndex]
+	leftHeight = leftHeightRange[leftHeightIndex]
+	rightHeight = rightHeightRange[rightHeightIndex]
+	startWall = startWallRange[startWallIndex]
+	endWall = endWallRange[endWallIndex]
+	curveForward = curveForwardRange[curveForwardIndex]
+	curveSideways = curveSidewaysRange[curveSidewaysIndex]
+
+func getStraightDict():
+	return {
+		"length": length,
+		"endOffset": endOffset,
+		"leftStartHeight": leftHeight,
+		"rightStartHeight": rightHeight,
+		"leftEndHeight": leftHeight,
+		"rightEndHeight": rightHeight,
+		"leftWallStart": startWall,
+		"rightWallStart": startWall,
+		"leftWallEnd": endWall,
+		"rightWallEnd": endWall,
+		"leftSmoothTilt": 3,
+		"rightSmoothTilt": 3,
+		"smoothOffset": 3,
+		"curve": false,
+	}
+
+func incrementStraigthIndeces():
+	rightHeightIndex += 1
+	if rightHeightIndex >= rightHeightRange.size():
+		rightHeightIndex = 0
+		leftHeightIndex += 1
+	if leftHeightIndex >= leftHeightRange.size():
+		leftHeightIndex = 0
+		endWallIndex += 1
+	if endWallIndex >= endWallRange.size():
+		endWallIndex = 0
+		startWallIndex += 1
+	if startWallIndex >= startWallRange.size():
+		startWallIndex = 0
+		lengthIndex += 1
+	if lengthIndex >= lengthRange.size(): # || curveIndex == 0:
+		lengthIndex = 0
+		endOffsetIndex += 1
+	if endOffsetIndex >= endOffsetRange.size(): # || curveIndex == 0:
+		endOffsetIndex = 0
+		curve = true
+
+func getCurveDict():
+	return {
+		"leftStartHeight": leftHeight,
+		"rightStartHeight": rightHeight,
+		"leftEndHeight": leftHeight,
+		"rightEndHeight": rightHeight,
+		"leftWallStart": startWall,
+		"rightWallStart": startWall,
+		"leftWallEnd": endWall,
+		"rightWallEnd": endWall,
+		"curve": true,
+		"leftSmoothTilt": 3,
+		"rightSmoothTilt": 3,
+		"smoothOffset": 3,
+		"curveForward": curveForward,
+		"curveSideways": curveSideways,
+	}
+
+func incrementCurveIndeces():
+	rightHeightIndex += 1
+	if rightHeightIndex >= rightHeightRange.size():
+		rightHeightIndex = 0
+		leftHeightIndex += 1
+	if leftHeightIndex >= leftHeightRange.size():
+		leftHeightIndex = 0
+		endWallIndex += 1
+	if endWallIndex >= endWallRange.size():
+		endWallIndex = 0
+		startWallIndex += 1
+	if startWallIndex >= startWallRange.size():
+		startWallIndex = 0
+		curveSidewaysIndex += 1
+	if curveSidewaysIndex >= curveSidewaysRange.size(): # || curveIndex == 1:
+		curveSidewaysIndex = 0
+		curveForwardIndex += 1
+	if curveForwardIndex >= curveForwardRange.size(): # || curveIndex == 1:
+		curveForwardIndex = 0
+		print("done")
+		done.emit()
+		queue_free()
+		visible = false
+
 func _process(_delta):
 	if (started > 0):
-		var length = lengthRange[lengthIndex]
-		var endOffset = endOffsetRange[endOffsetIndex]
-		var leftHeight = leftHeightRange[leftHeightIndex]
-		var rightHeight = rightHeightRange[rightHeightIndex]
-		var startWall = startWallRange[startWallIndex]
-		var endWall = endWallRange[endWallIndex]
+		prepareVariables()
 		var dict = {
 				"length": length,
 				"endOffset": endOffset,
@@ -74,122 +175,35 @@ func _process(_delta):
 		prefabMesher.decodeData(dict)
 		started -= 1
 		return
+	
+	getNextIcon()
+	# await newPrefab
+
+func preparePicture(dict: Dictionary):
+	prefabMesher.decodeData(dict)
+	camera.look_at(Vector3(0, 0, 0))
+	prefabMesher.global_position = -prefabMesher.getCenteringOffset()
+
+func getNextIcon(fresh: bool = false):
+	var category = 0
+	var dict = {}
+	if (rightHeightIndex == 1 && leftHeightIndex == 1):
+		if !curve:
+			incrementStraigthIndeces()
+		else:
+			incrementCurveIndeces()
+	prepareVariables()
 	if !curve: # straight only
-		# if leftHeightIndex == 1 && rightHeightIndex == 1:
-		# 	return
-		if !(rightHeightIndex == 1 && leftHeightIndex == 1):
-			var length = lengthRange[lengthIndex]
-			var endOffset = endOffsetRange[endOffsetIndex]
-			var leftHeight = leftHeightRange[leftHeightIndex]
-			var rightHeight = rightHeightRange[rightHeightIndex]
-			var startWall = startWallRange[startWallIndex]
-			var endWall = endWallRange[endWallIndex]
-
-
-			var dict = {
-				"length": length,
-				"endOffset": endOffset,
-				"leftStartHeight": leftHeight,
-				"rightStartHeight": rightHeight,
-				"leftEndHeight": leftHeight,
-				"rightEndHeight": rightHeight,
-				"leftWallStart": startWall,
-				"rightWallStart": startWall,
-				"leftWallEnd": endWall,
-				"rightWallEnd": endWall,
-				"leftSmoothTilt": 3,
-				"rightSmoothTilt": 3,
-				"smoothOffset": 3,
-				"curve": false,
-			}
-
-			prefabMesher.decodeData(dict)
-			camera.look_at(Vector3(0, 0, 0))
-			prefabMesher.global_position = -prefabMesher.getCenteringOffset()
-
-			await RenderingServer.frame_post_draw
-			newPrefab.emit(0, prefabMesher.getStringName(), dict, ImageTexture.create_from_image(subViewport.get_texture().get_image()))
-
-		rightHeightIndex += 1
-		if rightHeightIndex >= rightHeightRange.size():
-			rightHeightIndex = 0
-			leftHeightIndex += 1
-		if leftHeightIndex >= leftHeightRange.size():
-			leftHeightIndex = 0
-			endWallIndex += 1
-		if endWallIndex >= endWallRange.size():
-			endWallIndex = 0
-			startWallIndex += 1
-		if startWallIndex >= startWallRange.size():
-			startWallIndex = 0
-			lengthIndex += 1
-		if lengthIndex >= lengthRange.size(): # || curveIndex == 0:
-			lengthIndex = 0
-			endOffsetIndex += 1
-		if endOffsetIndex >= endOffsetRange.size(): # || curveIndex == 0:
-			endOffsetIndex = 0
-			curve = true
-			# done = true
-			# print("done")
-			# queue_free()
-			# visible = false
+		dict = getStraightDict()
+		category = 0
+		incrementStraigthIndeces()
 	else:
-		# if leftHeightIndex == 1 && rightHeightIndex == 1:
-		# 	return
-		if !(rightHeightIndex == 1 && leftHeightIndex == 1):
-			var leftHeight = leftHeightRange[leftHeightIndex]
-			var rightHeight = rightHeightRange[rightHeightIndex]
-			var startWall = startWallRange[startWallIndex]
-			var endWall = endWallRange[endWallIndex]
-			var curveForward = curveForwardRange[curveForwardIndex]
-			var curveSideways = curveSidewaysRange[curveSidewaysIndex]
-
-
-			var dict = {
-				"leftStartHeight": leftHeight,
-				"rightStartHeight": rightHeight,
-				"leftEndHeight": leftHeight,
-				"rightEndHeight": rightHeight,
-				"leftWallStart": startWall,
-				"rightWallStart": startWall,
-				"leftWallEnd": endWall,
-				"rightWallEnd": endWall,
-				"curve": true,
-				"leftSmoothTilt": 3,
-				"rightSmoothTilt": 3,
-				"smoothOffset": 3,
-				"curveForward": curveForward,
-				"curveSideways": curveSideways,
-			}
-
-			prefabMesher.decodeData(dict)
-			camera.look_at(Vector3(0, 0, 0))
-			prefabMesher.global_position = -prefabMesher.getCenteringOffset()
-
-			await RenderingServer.frame_post_draw
-			newPrefab.emit(1, prefabMesher.getStringName(), dict, ImageTexture.create_from_image(subViewport.get_texture().get_image()))
-
-		rightHeightIndex += 1
-		if rightHeightIndex >= rightHeightRange.size():
-			rightHeightIndex = 0
-			leftHeightIndex += 1
-		if leftHeightIndex >= leftHeightRange.size():
-			leftHeightIndex = 0
-			endWallIndex += 1
-		if endWallIndex >= endWallRange.size():
-			endWallIndex = 0
-			startWallIndex += 1
-		if startWallIndex >= startWallRange.size():
-			startWallIndex = 0
-			curveSidewaysIndex += 1
-		if curveSidewaysIndex >= curveSidewaysRange.size(): # || curveIndex == 1:
-			curveSidewaysIndex = 0
-			curveForwardIndex += 1
-		if curveForwardIndex >= curveForwardRange.size(): # || curveIndex == 1:
-			curveForwardIndex = 0
-			print("done")
-			done.emit()
-			queue_free()
-			visible = false
+		dict = getCurveDict()
+		category = 1
+		incrementCurveIndeces()
+	
+	preparePicture(dict)
+	await RenderingServer.frame_post_draw
+	newPrefab.emit(category, prefabMesher.getStringName(), dict, ImageTexture.create_from_image(subViewport.get_texture().get_image()))
 
 
