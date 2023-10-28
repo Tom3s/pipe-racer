24962dfd7182243719d44a83d9a7a2a60a55e3a2
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-10-28 19:49:46 +0300

    Better wheel visuals

--- tire.gd
+++ tire.gd
@@ -4,6 +4,7 @@ class_name Tire
 var targetRotation: float = 0.0
 
 
+var visualRotationNode = null
 var tireModel = null
 var smokeEmitter: GPUParticles3D  = null
 var dirtEmitter: GPUParticles3D = null
@@ -17,10 +18,11 @@ var tireMass: float = 20.0
 var tireIndex: int = 0
 
 @export
-var visualRotation: float = 2.0
+var visualRotation: float = 3.0
 
 func _ready():
-	tireModel = get_child(0)
+	visualRotationNode = get_child(0)
+	tireModel = get_child(0).get_child(0)
 	smokeEmitter = get_child(1)
 	smokeEmitter.emitting = false
 	smokeEmitter.one_shot = false
@@ -28,3 +30,6 @@ func _ready():
 	dirtEmitter.emitting = false
 	dirtEmitter.one_shot = false
 	set_physics_process(true)
+
+func _physics_process(delta):
+	visualRotationNode.rotation.y = rotation.y * visualRotation
