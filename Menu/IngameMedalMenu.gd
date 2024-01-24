ea0b8c6e75777987d1f1d2fd8a4a12865fec68e8
Tom3s <tomesjerry3@gmail.com>
2024-01-24 15:04:04 +0200

    Ingame Medal Menu Partially Done

---
+++ Menu/IngameMedalMenu.gd
@@ -0,0 +1,12 @@
+extends Control
+class_name IngameMedalMenu
+
+@onready var chrono: TextureRect = %Chrono
+@onready var goldTotal: TextureRect = %GoldTotal
+@onready var silverTotal: TextureRect = %SilverTotal
+@onready var bronzeTotal: TextureRect = %BronzeTotal
+
+@onready var blitz: TextureRect = %Blitz
+@onready var goldLap: TextureRect = %GoldLap
+@onready var silverLap: TextureRect = %SilverLap
+@onready var bronzeLap: TextureRect = %BronzeLap
