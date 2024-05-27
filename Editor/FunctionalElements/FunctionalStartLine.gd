596ec0b20d83a0bb812f07e6f0a0c92b4314a9f5
Tom3s <tomesjerry3@gmail.com>
2024-05-28 00:33:00 +0300

    Hotfix + docs

--- Editor/FunctionalElements/FunctionalStartLine.gd
+++ Editor/FunctionalElements/FunctionalStartLine.gd
@@ -81,7 +81,7 @@ func calculateRaycast(origin: Vector3):
 
 	var to = origin - global_transform.basis.y * RAYCAST_MAX_DISTANCE
 
-	var result = spaceState.intersect_ray(PhysicsRayQueryParameters3D.create(origin, to, 1))
+	var result = spaceState.intersect_ray(PhysicsRayQueryParameters3D.create(origin, to, 1 + 32))
 
 	if result.has("position"):
 		raycastPosition = result.position
